a4d1c304a126d7075b3fa974f2f608d01b101c81
Christoph Ungricht <christoph.ungricht@outlook.com>
2024-02-11 17:37:21 +0100

    feat: Display temperature and voltage on display.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- flash.sh
+++ flash.sh
@@ -2,8 +2,11 @@
 
 set -euxo pipefail
 
-# Build the project
+echo "Setting up permissions for /dev/ttyACM0..."
+sudo chmod 777 /dev/ttyACM0
+
+echo "Building the project..."
 arduino-cli compile --fqbn arduino:samd:mkr1000 tempdisp.ino
 
-# Flash the project
-sudo chmod 777 /dev/ttyACM0 && arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:samd:mkr1000 tempdisp.ino
+echo "Uploading the project..."
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:samd:mkr1000 tempdisp.ino
--- tempdisp.ino
+++ tempdisp.ino
@@ -1,17 +1,15 @@
 #include <LiquidCrystal.h>
+#include <avr/dtostrf.h>
+
+#define PERIOD_MS (500)
+#define TEMPERATURE_PIN (A1)
+#define TEMP_SCALE_FACTOR (10.0f) // 10mV per degree C
+#define CALIB_TEMP_VOLTAGE_MV_OFFSET (870.0f) // at value below
+#define CALIB_TEMP_C_OFFSET (23.3f) // at value above
 
 // Setup the LiquidCrystal library with the pin numbers we have
 // physically connected the module to.
 LiquidCrystal lcd(0, 1, 2, 3, 4, 5);
-
-int lastUpdate = 0;
-int currentIndex = 0;
-char* lyrics[] = {
-  "Drink all the...",
-  "     BOOZE!     ",
-  "Hack all the... ",
-  "    THINGS!     "
-};
 
 void setup() {
   // Setup the number of columns and rows that are available on the LCD.
@@ -19,45 +17,30 @@ void setup() {
 }
 
 void loop() {
-  // Get the time in milliseconds since the last reset.
-  int time = millis();
+    const int start_time = millis();
+    const int sensor_value = analogRead(TEMPERATURE_PIN);
+    const int voltage_mv = sensor_value * 3300 / 1023;
+    char voltage_disp_str[16];
+    snprintf(voltage_disp_str, sizeof(voltage_disp_str), "Voltage: %dmV", voltage_mv);
+    
+    const float temperature_c = (voltage_mv - CALIB_TEMP_VOLTAGE_MV_OFFSET) / TEMP_SCALE_FACTOR + CALIB_TEMP_C_OFFSET;
 
-  // If 800 milliseconds have passed since we last updated
-  // the text on the screen, print the next line of the
-  // lyrics on the screen.
-  if ((time  - lastUpdate) >= 800)
-  {
-    // Move the cursor back to the first column of the first row.
+    char temp_c_str[16];
+    dtostrf(temperature_c, 4, 1, temp_c_str);
+
+    char temp_disp_str[16];
+    snprintf(temp_disp_str, sizeof(temp_disp_str), "Temp: %s degC", temp_c_str);
+    lcd.clear();
     lcd.setCursor(0, 0);
+    lcd.print(temp_disp_str);
+    lcd.setCursor(0, 1);
+    lcd.print(voltage_disp_str);
+    
 
-    // If we are writing "Drink all the..." or "Hack all the..."
-    // then clear the screen and print the appropriate line.
-    if (currentIndex == 0 || currentIndex == 2)
-    {
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print(lyrics[currentIndex]);
-    }
-    else
-    {
-      // If we are writing the second line, move the cursor there
-      // and print the appropriate line.
-      lcd.setCursor(0, 1);
-      lcd.print(lyrics[currentIndex]);
+    const int end_time = millis();
+    const int elapsed_time = end_time - start_time;
+    if (elapsed_time < PERIOD_MS) {
+        delay(PERIOD_MS - elapsed_time);
     }
 
-    // Increment or reset the current index.
-    if (currentIndex == 3)
-    {
-      currentIndex = 0;
-    }
-    else
-    {
-      currentIndex += 1;
-    }
-
-    // Update the time that we last refreshed the screen to track
-    // when to update it again.
-    lastUpdate = time;
-  }
 }
